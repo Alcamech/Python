887f6bd5934f622c839af6890cb04af8e133928d
Lawton <Alcamech@gmail.com>
2014-08-01 14:23:58 -0400

    added two new quotes

--- FortuneCookie.py
+++ FortuneCookie.py
@@ -11,7 +11,7 @@ import random
 print("Welcome!\n")
 print("Here is your random positive message for the day...\n") 
 
-positive_message = random.randint(1,5) 
+positive_message = random.randint(1,7) 
 
 if positive_message == 1: 
 	print("Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time. -Thomas A. Edison")
@@ -24,4 +24,7 @@ elif positive_message == 4:
 	print("Always do your best. What you plant now, you will harvest later. -Og Mandino")
 elif positive_message == 5:
 	print("Expect problems and eat them for breakfast. -Alfred A. Montapert")
-
+elif positive_message == 6: 
+	print("The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence. -Confucius")
+elif positive_message == 7:
+	print("Infuse your life with action. Don't wait for it to happen. Make it happen. Make your own future. Make your own hope. Make your own love. And whatever your beliefs, honor your creator, not by passively waiting for grace to come down from upon high, but by doing what you can to make grace happen... yourself, right now, right down here on Earth. -Bradley Whitford") 
